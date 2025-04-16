874c65914739d0fbe235a3e7b223078f9f2d85e7
Axel Diego <axelds05@outlook.com>
2025-04-16 15:37:47 -0600

    First commit

---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  imports: [],
+  templateUrl: './loading-spinner.component.html',
+})
+export class LoadingSpinnerComponent {
+
+}
